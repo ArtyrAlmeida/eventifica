3cc1662a2dccf9c210c180deed6739a45387f21a
Immanuel-Victor <immanuelvictor.a.f@gmail.com>
2023-04-26 16:57:40 -0300

    fix: adiciona cors ao projeto

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,10 +1,12 @@
 import express, { Request, Response } from 'express';
 import { eventsRouter } from './routes/routes';
 import connectDb from './database/database';
+import cors from 'cors';
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 app.get('/', (req: Request, res: Response) => {
     res.json('Running');
